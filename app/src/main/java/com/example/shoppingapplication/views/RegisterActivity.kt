64955c068c6aaccb35d310e5a5874493bbb5b32e
jaber <jaberqasem75@gmail.com>
2023-07-22 18:27:58 +0300

    Build auth views and VerificationActivity

--- app/src/main/java/com/example/shoppingapplication/views/RegisterActivity.kt
+++ app/src/main/java/com/example/shoppingapplication/views/RegisterActivity.kt
@@ -3,10 +3,13 @@ package com.example.shoppingapplication.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoppingapplication.R
+import com.example.shoppingapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
