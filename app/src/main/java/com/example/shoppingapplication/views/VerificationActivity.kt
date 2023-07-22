6d1887c801e7ed4f33e4b602c2bf809c8433d453
jaber <jaberqasem75@gmail.com>
2023-07-22 17:34:04 +0300

    Build auth views and VerificationActivity

---
+++ app/src/main/java/com/example/shoppingapplication/views/VerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingapplication.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppingapplication.R
+
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
